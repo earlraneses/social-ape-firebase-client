9e072fbd582b93f6d20e7cde43de24e5f335103e
earlraneses <33829405+earlraneses@users.noreply.github.com>
2020-07-17 17:04:03 +0800

    Add files via upload

---
+++ src/components/profile/EditDetails.js
@@ -0,0 +1,143 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+// Redux
+import { connect } from "react-redux";
+import { editUserDetails } from "../../redux/actions/userActions";
+// MUI stuff
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+// Custom UI
+import MyButton from "../../util/MyButton";
+// Icons
+import EditIcon from "@material-ui/icons/Edit";
+
+const styles = (theme) => ({
+  ...theme.nonPalette,
+  button: {
+    float: "right",
+  },
+});
+
+class EditDetails extends Component {
+  state = {
+    bio: "",
+    website: "",
+    location: "",
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({ open: true });
+    this.mapUserDetailsToState(this.props.credentials);
+  };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  componentDidMount() {
+    this.mapUserDetailsToState(this.props.credentials);
+  }
+  mapUserDetailsToState = (credentials) => {
+    this.setState({
+      bio: credentials.bio || "",
+      website: credentials.website || "",
+      location: credentials.location || "",
+    });
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  handleSubmit = () => {
+    const userDetails = {
+      bio: this.state.bio,
+      website: this.state.website,
+      location: this.state.location,
+    };
+    this.props.editUserDetails(userDetails);
+    this.handleClose();
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <MyButton
+          tip="Edit details"
+          onClick={this.handleOpen}
+          btnClassName={classes.button}
+        >
+          <EditIcon color="primary" />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <DialogTitle>Edit your details</DialogTitle>
+          <DialogContent>
+            <form>
+              <TextField
+                name="bio"
+                type="text"
+                label="Bio"
+                multiline
+                rows="3"
+                placeholder="A short bio of yourself"
+                className={classes.textField}
+                value={this.state.bio}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="website"
+                type="text"
+                label="Website"
+                placeholder="Your website"
+                className={classes.textField}
+                value={this.state.website}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="location"
+                type="text"
+                label="Location"
+                placeholder="Where you live"
+                className={classes.textField}
+                value={this.state.location}
+                onChange={this.handleChange}
+                fullWidth
+              />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleSubmit} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  credentials: state.user.credentials,
+});
+
+EditDetails.propTypes = {
+  classes: PropTypes.object.isRequired,
+  editUserDetails: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, { editUserDetails })(
+  withStyles(styles)(EditDetails)
+);
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,160 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+// Redux
+import { connect } from "react-redux";
+import { logoutUser, uploadImage } from "../../redux/actions/userActions";
+// MUI stuff
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+// Custom UI
+import MyButton from "../../util/MyButton";
+// Icons
+import LocationOn from "@material-ui/icons/LocationOn";
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import LinkIcon from "@material-ui/icons/Link";
+import EditIcon from "@material-ui/icons/Edit";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+// Components
+import EditDetails from "./EditDetails";
+import ProfileSkeleton from "../../util/ProfileSkeleton";
+
+const styles = (theme) => ({
+  ...theme.nonPalette,
+});
+
+class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    this.props.uploadImage(formData);
+  };
+  handleEditPicture = () => {
+    const fileInput = document.getElementById("imageInput");
+    fileInput.click();
+  };
+  handleLogout = () => {
+    this.props.logoutUser();
+  };
+  render() {
+    const {
+      classes,
+      user: {
+        credentials: { handle, createdAt, imageUrl, bio, website, location },
+        loading,
+        authenticated,
+      },
+    } = this.props;
+
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className="image-wrapper">
+              <img src={imageUrl} alt="profile" className="profile-image" />
+              <input
+                type="file"
+                id="imageInput"
+                hidden="hidden"
+                onChange={this.handleImageChange}
+              />
+              <MyButton
+                tip="Edit profile picture"
+                onClick={this.handleEditPicture}
+                btnClassName="button"
+              >
+                <EditIcon color="primary" />
+              </MyButton>
+            </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="primary"
+                variant="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LocationOn color="primary" /> <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              {website && (
+                <Fragment>
+                  <LinkIcon color="primary" />
+                  <a href={website} target="_blank" rel="noopener noreferrer">
+                    {" "}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarToday color="primary" />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </div>
+            <MyButton tip="Logout" onClick={this.handleLogout}>
+              <ExitToApp color="primary" />
+            </MyButton>
+            <EditDetails />
+          </div>
+        </Paper>
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+      <ProfileSkeleton />
+    );
+    return profileMarkup;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapActionsToProps = { logoutUser, uploadImage };
+
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Profile));
